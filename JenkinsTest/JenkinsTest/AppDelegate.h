af4d82b80c2b71d7467567382874bcfc127030df
Dani Postigo <dani.postigo@gmail.com>
2014-08-15 11:54:04 -0400

    change

---
+++ JenkinsTest/JenkinsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by Danielle Postigo on 8/15/14.
+//  Copyright (c) 2014 American Express. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JenkinsTest/JenkinsTestTests/JenkinsTestTests.m
@@ -0,0 +1,34 @@
+//
+//  JenkinsTestTests.m
+//  JenkinsTestTests
+//
+//  Created by Danielle Postigo on 8/15/14.
+//  Copyright (c) 2014 American Express. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JenkinsTestTests : XCTestCase
+
+@end
+
+@implementation JenkinsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
